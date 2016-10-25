2677f55959b8bd37a691133b9591bf435b7205c8
idhammath <idham.math@gmail.com>
2016-10-25 18:00:23 +0700

    Create matematiks.js

---
+++ matematiks.js
@@ -0,0 +1,2 @@
+function buatkanvas(tempatKanvas,namaKanvas='canvas',lebar='200',tinggi='200'){
+  document.getElementById(tempatKanvas).innerHTML="<canvas id="+namaKanvas+" width="+lebar+" height="+tinggi+"></canvas>"
