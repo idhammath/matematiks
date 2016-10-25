c3e283c75ead0fd0b8cd58e06af92bd82b2a7bd1
idhammath <idham.math@gmail.com>
2016-10-25 18:30:44 +0700

    Update matematiks.js

--- matematiks.js
+++ matematiks.js
@@ -1,2 +1,5 @@
 function buatkanvas(tempatKanvas,namaKanvas='canvas',lebar='200',tinggi='200'){
-  document.getElementById(tempatKanvas).innerHTML="<canvas id="+namaKanvas+" width="+lebar+" height="+tinggi+"></canvas>"
+  document.getElementById(tempatKanvas).innerHTML="<canvas id="+namaKanvas+" width="+lebar+" height="+tinggi+"></canvas>";
+  kanpas = document.getElementById(namaKanvas).getContext('2d');
+  kanpas.transform(-1,1,1,1,0,Number(tinggi));
+}
