b6a11e7f73cf30fca5a6cd85f544bb9d6af7e111
idhammath <idham.math@gmail.com>
2016-10-26 10:31:18 +0700

    Update matematiks.js

--- matematiks.js
+++ matematiks.js
@@ -2,4 +2,16 @@ function buatkanvas(tempatKanvas,namaKanvas='canvas',lebar='200',tinggi='200'){
   document.getElementById(tempatKanvas).innerHTML="<canvas id="+namaKanvas+" width="+lebar+" height="+tinggi+"></canvas>";
   kanpas = document.getElementById(namaKanvas).getContext('2d');
   kanpas.transform(-1,1,1,1,0,Number(tinggi));
-}
+};
+function persegi(idCanvas,titikAwal,panjangSisi){
+	var kanvas = document.getElementById(idCanvas);
+	var konteks = kanvas.getContext("2d");
+  if (panjangSisi==0) return;
+  konteks.beginPath();
+  konteks.moveTo(titikAwal[0],titikAwal[1]);
+  konteks.lineTo(titikAwal[0]+panjangSisi,titikAwal[1]);
+  konteks.lineTo(titikAwal[0]+panjangSisi,titikAwal[1]-panjangSisi);
+  konteks.lineTo(titikAwal[0],titikAwal[1]-panjangSisi);
+  konteks.closePath();
+  konteks.stroke();
+};
